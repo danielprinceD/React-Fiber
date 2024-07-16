976afdb893ec7d520084f8d7f06f704749be6760
Daniel Prince <d.danielprince2003@gmail.com>
2024-07-16 20:25:08 +0530

    feat:added leva controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import MyCanvas from "./Three/MyCanvas";
+import { Leva } from "leva";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Leva />
       <MyCanvas />
     </>
   );
--- src/Three/MyCanvas.jsx
+++ src/Three/MyCanvas.jsx
@@ -5,15 +5,26 @@ import MySphere from "./MySphere";
 import MyTorus from "./MyTorus";
 import { OrbitControls, useHelper } from "@react-three/drei";
 import { DirectionalLightHelper, PlaneGeometry, PlaneHelper } from "three";
+import { useControls } from "leva";
 
 const Scene = () => {
   const lightRef = useRef();
+  const { matColor, intensity } = useControls({
+    matColor: {
+      value: "green",
+    },
+    intensity: {
+      value: 0.5,
+      min: 0,
+      max: 5,
+    },
+  });
   useHelper(lightRef, DirectionalLightHelper, 0.5, "black");
   return (
     <>
       <OrbitControls />
-      <directionalLight position={[5, 5, 10]} ref={lightRef} />
-      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 10]} ref={lightRef} color={matColor} />
+      <ambientLight intensity={intensity} />
 
       <MyMesh position={[-2, 0, 0]} />
       <MySphere position={[0, 0, 0]} />
