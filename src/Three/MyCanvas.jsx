a43f03e38ab1cd07e46456ec68bb7424a20181ad
Daniel Prince <d.danielprince2003@gmail.com>
2024-07-16 19:53:52 +0530

    feat:added drei orbit controls

--- src/Three/MyCanvas.jsx
+++ src/Three/MyCanvas.jsx
@@ -3,15 +3,17 @@ import React from "react";
 import MyMesh from "./MyMesh";
 import MySphere from "./MySphere";
 import MyTorus from "./MyTorus";
+import { OrbitControls } from "@react-three/drei";
 
 const MyCanvas = () => {
   return (
     <Canvas>
+      <OrbitControls />
       <directionalLight position={[5, 5, 10]} />
       <ambientLight intensity={0.3} />
-      {/* <MyMesh position={[-2, 0, 0]} /> */}
+      <MyMesh position={[-2, 0, 0]} />
       <MySphere position={[0, 0, 0]} />
-      {/* <MyTorus position={[0, 0, 0]} /> */}
+      <MyTorus position={[0, 0, 0]} />
     </Canvas>
   );
 };
